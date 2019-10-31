c3dcf1d3a3ec809acbae8c2f61f1c234e9c677bb
Nishchit14 <dnishchit@gmail.com>
2019-10-31 09:55:20 +0530

    Poject Added

---
+++ app.js
@@ -0,0 +1,31 @@
+const {
+  app,
+  bw
+} = require("./scripts/modules")
+
+
+let win
+
+let createWindow = () => {
+
+  win = new bw({
+    width: 600,
+    height: 600,
+    show: false
+  })
+
+  win.loadURL(`file://${__dirname}/index.html`)
+
+  win.on("ready-to-show", () => {
+    win.show()
+    win.maximize()
+  })
+
+  win.webContents.openDevTools()
+
+  win.on("closed", () => {
+    win = null
+  })
+}
+
+app.on("ready", createWindow)
